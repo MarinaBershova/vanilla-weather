f76ed66c895d123b14a31295aca79cfabc29bba3
Marina <marinabershova@gmail.com>
2022-07-18 14:02:03 +0200

    Showing the current temperature, city, discription,wind

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
+let apiKey = "a350fa7c4ad24a3f0d0e06b5f5f554bf";
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
